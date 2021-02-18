a7190962002f1926c60343a20070afa9664ba836
Thomas Horn Sivertsen <thomass@cerpus.com>
2021-02-18 12:36:55 +0100

    Added support for setting values via the config function

--- src/Helpers/Profile.php
+++ src/Helpers/Profile.php
@@ -5,7 +5,7 @@ namespace Cerpus\Helper\Helpers;
 
 function profile($key, $default = null, $requiredProfile = null) {
     $profile = $requiredProfile ?? config('app.deploymentEnvironment');
-    if( !is_null($profile) ){
+    if( !is_null($profile) && !is_array($key)){
         return config($profile . "." . $key, config($key, $default));
     }
     return config($key, $default);
